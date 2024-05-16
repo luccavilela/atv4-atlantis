35b47f27e41776d7137f45122732d2d8c24c1239
luccavilela <luccaloyale@gmail.com>
2024-05-16 14:36:09 -0300

    Edição no formulário

--- atv4/src/componentes/cadastro/formularioCadastroClienteDependente.tsx
+++ atv4/src/componentes/cadastro/formularioCadastroClienteDependente.tsx
@@ -11,7 +11,6 @@ interface FormularioCadastroClienteDependenteProps {
     seletorView: (valor: string, e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-
 export default function FormularioCadastroClienteDependente(props: FormularioCadastroClienteDependenteProps){
     const [clientes, setClientes] = useState<Array<Cliente>>([]);
     const [titularSelecionado, setTitularSelecionado] = useState<string>('');
